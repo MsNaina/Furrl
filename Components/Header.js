41e924ffedc94416f25723b41d72848bf121f153
Ms.Naina <nainachhapola2706@gmail.com>
2025-06-24 09:11:00 +0530

    edit blue

--- Components/Header.js
+++ Components/Header.js
@@ -7,62 +7,25 @@ const homeGirl = "/homeGirl.png";
 const BlueGirl="/Bluelady.png"
 const flower="/flower.png"
 const orange ="/orangeStuff.png"
-const orangebg= "/Orange.png"
-const bg="/bg.jpg"
+const orangebg = "/Orange.png"
 
 const Header = () => {
 
   const [phase, setPhase] = useState('before');
   const [showBubble, setShowBubble] = useState(false);
 
+  useEffect(() => {
+    setShowBubble(true);
+  }, []);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setShowBubble(true);
-  //   }, 100);
-  // }, []);
-  
-
-  //   useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setPhase('after');
-  //   }, 2000);
-  //   return () => clearTimeout(timeout);
-  //   }, []);
-  
+  const handleBubbleEnd = () => {
+    setPhase("after");
+  };
 
   return (
     <>
-      <div className="grid">
-        <div
-          className={`gradient-layer ${showBubble ? "bubble-wipe" : ""}`}
-        ></div>
-
-        <div className="bubble-overlay"></div>
-
-        {/* <div id="home-text" className={`home-text ${phase}`}>
-          {phase === "before" ? (
-            <>
-              <h1 className="home-heading">Welcome</h1>
-              <h1 className="heading-transition">
-                {" "}
-                <span className="signature-text">to a</span> World <br />{" "}
-                <span className="of-text">of</span>
-              </h1>
-            </>
-          ) : (
-            <h1 className="heading-transition">
-              <span className="signature-text">stylish</span> Homegrown <br />
-              <span className="of-text">brand</span>
-            </h1>
-          )}
-        </div>
-
-        <img
-          src={phase === "before" ? homeGirl : BlueGirl}
-          className={phase === "before" ? "pinkLady" : "blueLady"}
-          alt="girl"
-        /> */}
+      <div className={`grid ${phase === "after" ? "after-bubble after" : ""}`}>
+        <div className="bubble-overlay" onAnimationEnd={handleBubbleEnd}></div>
         {phase === "before" ? (
           <>
             <div id="home-text">
@@ -76,10 +39,10 @@ const Header = () => {
           </>
         ) : (
           <>
-            <div id="home-text">
-              <h1 className="heading-transition">
-                <span className="signature-text">stylish</span> Homegrown <br />
-                <span className="of-text">brand</span>
+            <div id="home-text" className="home-text-blue">
+              <h1 className="heading-transition-blue">
+                <span className="signature-text-style">Stylish</span> Homegrown <br />
+                <span className="of-text">brands</span>
               </h1>
             </div>
             <img src={BlueGirl} className="blueLady" />
@@ -88,13 +51,16 @@ const Header = () => {
 
         <div className="flower-orange">
           <img src={flower} className="flower"></img>
-          <img src={orangebg} className="orange-bg"></img>
+          <img
+            src={orangebg}
+            className={`orange-bg ${phase === "after" ? "after" : ""}`}
+          ></img>
         </div>
 
         <img src={orange} className="orange"></img>
 
         <div>
-          <MobileMockup/>
+          <MobileMockup />
         </div>
       </div>
     </>
